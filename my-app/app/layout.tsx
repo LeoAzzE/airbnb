5db7d65eb7fae0f557411576e930cacfab201aef
Borges dev <leosull50@hotmail.com>
2024-02-14 18:24:06 -0300

    startando o clone

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,27 @@
+import { Nunito } from "next/font/google"
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar"
+
+export const metadata = {
+  title: "Airbnb",
+  description: "AirBnb clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"]
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        </body>
+    </html>
+  );
+}
